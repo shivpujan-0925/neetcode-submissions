79944f363f720e301b9324eec7041bf67376dc58
shivpujan <kumarshivpujan9087@gmail.com>
2026-07-14 17:16:03 +0530

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n == 0) return 1;
+
+        if(n > 0) {
+            return Math.pow(x,n);
+        }else{
+            double X = 1/x;
+            long N = -n;
+
+            return Math.pow(X,N);
+        }
+    }
+}
